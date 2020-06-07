7f482c220eb61e204bf9ba32bcda99ff69546844
VasylievHennadii <vga.kha@gmail.com>
2020-06-08 01:53:05 +0300

    brand output

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -13,8 +13,11 @@ use ishop\Cache;
  */
 class MainController extends AppController {   
 
-    public function indexAction(){        
-        $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');     
+    public function indexAction(){  
+        //$brands = \R::findAll('brand'); //получаем все бренды из БД
+        $brands = \R::find('brand', 'LIMIT 3');//получаем 3 бренда из БД        
+        $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');  
+        $this->set(compact('brands'));//передаем бренды в вид   
     }
 
 }
--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -18,40 +18,26 @@
 	<!--banner-ends--> 
 
 	<!--about-starts-->
+	<?php if($brands) : ?>
 	<div class="about"> 
 		<div class="container">
 			<div class="about-top grid-1">
-				<div class="col-md-4 about-left">
-					<figure class="effect-bubba">
-						<img class="img-responsive" src="images/abt-1.jpg" alt=""/>
-						<figcaption>
-							<h2>Nulla maximus nunc</h2>
-							<p>In sit amet sapien eros Integer dolore magna aliqua</p>	
-						</figcaption>			
-					</figure>
-				</div>
-				<div class="col-md-4 about-left">
-					<figure class="effect-bubba">
-						<img class="img-responsive" src="images/abt-2.jpg" alt=""/>
-						<figcaption>
-							<h4>Mauris erat augue</h4>
-							<p>In sit amet sapien eros Integer dolore magna aliqua</p>	
-						</figcaption>			
-					</figure>
-				</div>
-				<div class="col-md-4 about-left">
-					<figure class="effect-bubba">
-						<img class="img-responsive" src="images/abt-3.jpg" alt=""/>
-						<figcaption>
-							<h4>Cras elit mauris</h4>
-							<p>In sit amet sapien eros Integer dolore magna aliqua</p>	
-						</figcaption>			
-					</figure>
-				</div>
+				<?php foreach($brands as $brand) : ?>
+					<div class="col-md-4 about-left">
+						<figure class="effect-bubba">
+							<img class="img-responsive" src="images/<?=$brand->img;?>" alt=""/>
+							<figcaption>
+								<h2><?=$brand->title;?></h2>
+								<p><?=$brand->description;?></p>	
+							</figcaption>			
+						</figure>
+					</div>
+				<?php endforeach; ?>
 				<div class="clearfix"></div>
 			</div>
 		</div>
 	</div>
+	<?php endif; ?>
 	<!--about-end-->
 	<!--product-starts-->
 	<div class="product"> 
