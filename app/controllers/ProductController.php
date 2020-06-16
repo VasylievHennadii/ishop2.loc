ca91738b87c0d9dd68dbd93abf2dd7127eb26e86
VasylievHennadii <vga.kha@gmail.com>
2020-06-16 15:35:51 +0300

    Card Product. Gallery Pictures.

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -30,11 +30,13 @@ class ProductController extends AppController {
         //просмотренные товары
 
         //галерея
+        $gallery = \R::findAll('gallery', 'product_id = ?', [$product->id]);
+        
 
         //модификации товара
 
         $this->setMeta($product->title, $product->description, $product->keywords);//установка метаданных
-        $this->set(compact('product', 'related'));
+        $this->set(compact('product', 'related','gallery'));
     }
 
 }
--- app/views/Product/view.php
+++ app/views/Product/view.php
@@ -17,19 +17,19 @@
             <div class="col-md-9 single-main-left">
                 <div class="sngl-top">
                     <div class="col-md-5 single-top-left">	
+                        <?php if($gallery) : ?>
                         <div class="flexslider">
-                                <ul class="slides">
-                                <li data-thumb="images/s-1.jpg">
-                                    <div class="thumb-image"> <img src="images/s-1.jpg" data-imagezoom="true" class="img-responsive" alt=""/> </div>
-                                </li>
-                                <li data-thumb="images/s-2.jpg">
-                                        <div class="thumb-image"> <img src="images/s-2.jpg" data-imagezoom="true" class="img-responsive" alt=""/> </div>
-                                </li>
-                                <li data-thumb="images/s-3.jpg">
-                                    <div class="thumb-image"> <img src="images/s-3.jpg" data-imagezoom="true" class="img-responsive" alt=""/> </div>
-                                </li> 
-                                </ul>
-                        </div>                    
+                            <ul class="slides">
+                                <?php foreach($gallery as $item) : ?>
+                                <li data-thumb="images/<?=$item->img;?>">
+                                    <div class="thumb-image"> <img src="images/<?=$item->img;?>" data-imagezoom="true" class="img-responsive" alt=""/> </div>
+                                </li>    
+                                <?php endforeach; ?>
+                            </ul>
+                        </div> 
+                        <?php else: ?>
+                            <img src="images/<?=$product->img;?>" alt="">
+                        <?php endif;?>                   
                     </div>	
                     <?php 
                     $curr = \ishop\App::$app->getProperty('currency');
