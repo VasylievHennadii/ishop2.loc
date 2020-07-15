2c0a0b071d5aba995e4b8c6c83764f86dd466c14
VasylievHennadii <vga.kha@gmail.com>
2020-07-15 21:31:38 +0300

    Registration. Authorization

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -38,14 +38,24 @@ class UserController extends AppController {
      * Action метод для входа
      */
     public function loginAction(){
-        
+        if(!empty($_POST)){
+            $user = new User();
+            if($user->login()){
+                $_SESSION['success'] = 'Вы успешно авторизованы';
+            }else{
+                $_SESSION['error'] = 'Логин/пароль введены неверно';
+            }
+            redirect();
+        }
+        $this->setMeta('Вход');
     }
 
     /**
      * Action метод для выхода
      */
     public function logoutAction(){
-        
+        if(isset($_SESSION['user'])) unset($_SESSION['user']);
+        redirect();
     }
 
 }
--- app/models/User.php
+++ app/models/User.php
@@ -54,6 +54,30 @@ class User extends AppModel{
         return true;
     }
 
+    /**
+     * метод для авторизации, как с общей страницы, так и из админки
+     */
+    public function login($isAdmin = false){
+        $login = !empty(trim($_POST['login'])) ? trim($_POST['login']) : null;
+        $password = !empty(trim($_POST['password'])) ? trim($_POST['password']) : null;
+        if($login && $password){
+            if($isAdmin){
+                $user = \R::findOne('user', "login = ? AND role = 'admin'", [$login]);
+            }else{
+                $user = \R::findOne('user', "login = ? ", [$login]);
+            }
+            if($user){
+                if(password_verify($password, $user->password)){
+                    foreach($user as $k => $v){
+                        if($k != 'password') $_SESSION['user'][$k] = $v;
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
 
 
