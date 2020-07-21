053272ddf2dee570432230fd81b51864f23aa16e
VasylievHennadii <vga.kha@gmail.com>
2020-07-22 00:36:24 +0300

    Checkout. Part 4. Sending mail messages with Swift Mailer.

--- app/models/Order.php
+++ app/models/Order.php
@@ -2,6 +2,12 @@
 
 namespace app\models;
 
+use ishop\App;
+use Swift_Mailer;
+use Swift_Message;
+use Swift_SmtpTransport;
+
+
 
 /**
  * модель для работы с заказом
@@ -38,6 +44,39 @@ class Order extends AppModel {
      * метод отправляет письмо администратору магазина и клиенту
      */
     public static function mailOrder($order_id, $user_email){
+        // Create the Transport        
+        $transport = (new Swift_SmtpTransport(App::$app->getProperty('smtp_host'), App::$app->getProperty('smtp_port'), App::$app->getProperty('smtp_protocol')))
+        ->setUsername(App::$app->getProperty('smtp_login'))
+        ->setPassword(App::$app->getProperty('smtp_password'))
+        ;
+        // Create the Mailer using your created Transport
+        $mailer = new Swift_Mailer($transport);
+
+        //Create a message
+        ob_start();
+        require APP . '/views/mail/mail_order.php';
+        $body = ob_get_clean();
+
+        $message_client = (new Swift_Message("Вы сделали заказ №{$order_id} на сайте " . App::$app->getProperty('shop_name')))
+        ->setFrom([App::$app->getProperty('smtp_login') => App::$app->getProperty('shop_name')])
+        ->setTo($user_email)
+        ->setBody($body, 'text/html')
+        ;
+
+        $message_admin = (new Swift_Message("Сделан заказ №{$order_id} на сайте"))
+        ->setFrom([App::$app->getProperty('smtp_login') => App::$app->getProperty('shop_name')])
+        ->setTo(App::$app->getProperty('admin_email'))
+        ->setBody($body, 'text/html')
+        ;
+
+        // Send the message
+        $result = $mailer->send($message_client);        
+        $result = $mailer->send($message_admin);
+        unset($_SESSION['cart']);
+        unset($_SESSION['cart.qty']);
+        unset($_SESSION['cart.sum']);
+        unset($_SESSION['cart.currency']);
+        $_SESSION['success'] = 'Спасибо за Ваш заказ. В ближайшее время с Вами свяжется менеджер для согласования заказа';
 
     }
 
--- config/params.php
+++ config/params.php
@@ -8,6 +8,9 @@ return [
     'admin_email' => 'petpolimer@gmail.com',
     'shop_name' => 'Магазин ishop2.loc',
     'pagination' => 3,
-    'smtp_login' => '',
-    'smtp_password' => '',
+    'smtp_host' => 'smtp.ukr.net',
+    'smtp_port' => '2525',
+    'smtp_protocol' => 'ssl',
+    'smtp_login' => 'petpolimer@ukr.net',
+    'smtp_password' => 'EbmV3tF4JjgQu59s',
 ];
