0ad4c09d04921eeadced9b669567cd6086b840da
VasylievHennadii <vga.kha@gmail.com>
2020-08-01 00:01:03 +0300

    Admin Home Page Widgets

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -10,7 +10,12 @@ namespace app\controllers\admin;
 class MainController extends AppController{
 
     public function indexAction(){
+        $countNewOrders = \R::count('order', "status = '0'");
+        $countUsers = \R::count('user');
+        $countProducts = \R::count('product');
+        $countCategories = \R::count('category');
         $this->setMeta('Панель управления');
+        $this->set(compact('countNewOrders', 'countUsers', 'countProducts', 'countCategories'));
     }
 
 }
--- app/views/admin/Main/index.php
+++ app/views/admin/Main/index.php
@@ -16,14 +16,13 @@
             <!-- small box -->
             <div class="small-box bg-aqua">
                 <div class="inner">
-                    <h3>150</h3>
-
+                    <h3><?=$countNewOrders;?></h3>
                     <p>New Orders</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?=ADMIN;?>/order" class="small-box-footer">Все заказы <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
@@ -31,14 +30,13 @@
             <!-- small box -->
             <div class="small-box bg-green">
                 <div class="inner">
-                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                    <p>Bounce Rate</p>
+                    <h3><?=$countProducts;?></h3>
+                    <p>Products</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-stats-bars"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?=ADMIN;?>/product" class="small-box-footer">Все товары <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
@@ -46,14 +44,13 @@
             <!-- small box -->
             <div class="small-box bg-yellow">
                 <div class="inner">
-                    <h3>44</h3>
-
+                    <h3><?=$countUsers;?></h3>
                     <p>User Registrations</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-person-add"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?=ADMIN;?>/user" class="small-box-footer">Все пользователи <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
@@ -61,14 +58,14 @@
             <!-- small box -->
             <div class="small-box bg-red">
                 <div class="inner">
-                    <h3>65</h3>
+                    <h3><?=$countCategories;?></h3>
 
-                    <p>Unique Visitors</p>
+                    <p>Categories</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-pie-graph"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?=ADMIN;?>/category" class="small-box-footer">Все категории <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
